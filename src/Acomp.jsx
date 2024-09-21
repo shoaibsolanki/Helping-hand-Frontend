318135b02c6e8c412df7fc24ae6b4e2bf56d8c59
Veedel <hot.sa.chhora@gmail.com>
2024-09-21 19:04:00 +0530

    cross

--- src/Acomp.jsx
+++ src/Acomp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Acomp = () => {
-  return <div>Acompsdgfjdsgfsaashdsdfhbsdhjfb</div>;
+  return <div>Acompsdgfjdsgfsaashdsdfhbsdhjffghjdsfsfsb</div>;
 };
 
 export default Acomp;
